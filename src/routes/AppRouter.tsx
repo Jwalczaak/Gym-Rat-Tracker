29dc4c20681020303cd47174f985d03a8afc3086
Jan Walczak <j.walczaak@gmail.com>
2026-04-26 12:45:42 +0200

    Add nested content in appLayout as routes

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { routes } from './paths';
+import AppLayout from '../ui/AppLayout/AppLayout';
 
 export function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        {Object.values(routes).map((route) => (
-          <Route
-            key={route.path}
-            path={route.path}
-            element={<route.component />}
-          />
-        ))}
+        <Route element={<AppLayout />}>
+          {Object.values(routes).map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={<route.component />}
+            />
+          ))}
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/ui/AppLayout/AppLayout.tsx
+++ src/ui/AppLayout/AppLayout.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from '../Header/Header';
+import Sidebar from '../Sidebar/Sidebar';
 
 const AppLayout: React.FC = () => {
-  return <div> </div>;
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+      <main>
+        <div>
+          app layout
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default AppLayout;
