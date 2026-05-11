1fe44827314ce8b5b49cc4cf62a086de7dfc551c
Jan Walczak <j.walczaak@gmail.com>
2026-05-11 12:09:48 +0200

    Change position of element in MealCard component

--- src/features/diet/MealCard/MealCard.tsx
+++ src/features/diet/MealCard/MealCard.tsx
@@ -1,4 +1,7 @@
-import { Card, CardHeader } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import { FaRegTrashAlt } from 'react-icons/fa';
+import { CiEdit } from 'react-icons/ci';
 import type { Meal } from '@/types/meal';
 import React from 'react';
 import Chart from '@/components/shared/ Chart/Chart';
@@ -44,48 +47,52 @@ const MealCard: React.FC<MealCardProps> = ({ meal, onEdit, onDelete }) => {
   ];
 
   return (
-    <Card className="h-45 bg-gray-100">
-      <CardHeader className="flex h-full items-center justify-between">
-        <div className="content items-between flex h-full w-full flex-col justify-between font-semibold">
-          <div className="flex flex-col">
+    <Card className="h-45 gap-4 bg-gray-100">
+      <CardContent className="content flex h-full w-full items-center justify-between font-semibold">
+        <div className="flex h-full flex-col justify-between">
+          <div className="flex flex-col gap-1">
             <span className="text-2xl">{meal.name}</span>
             <span className="text-sm font-normal">{meal.weight}g</span>
           </div>
 
-          <span>
-            <div className="flex w-full items-start justify-between">
-              <div className="flex gap-3">
-                <div className="chart-container flex items-center gap-2 font-thin">
-                  <Chart
-                    type="donut"
-                    width={20}
-                    height={30}
-                    data={proteinData}
-                  />
-                  <span className="text-sm font-normal">
-                    {' '}
-                    {proteinData[0].value}g
-                  </span>
-                </div>
-                <div className="chart-container flex items-center gap-2 font-thin">
-                  <Chart type="donut" width={20} height={30} data={fatData} />
-                  <span className="text-sm font-normal">
-                    {' '}
-                    {fatData[0].value}g
-                  </span>
-                </div>{' '}
-                <div className="chart-container flex items-center gap-2 font-thin">
-                  <Chart type="donut" width={20} height={30} data={carbsData} />
-                  <span className="text-sm font-normal">
-                    {' '}
-                    {carbsData[0].value}g
-                  </span>
-                </div>
-              </div>
+          <div className="flex gap-3">
+            <div className="chart-container flex items-center gap-2 font-thin">
+              <Chart type="donut" width={20} height={30} data={proteinData} />
+              <span className="text-sm font-normal">
+                {' '}
+                {proteinData[0].value}g
+              </span>
             </div>
-          </span>
+            <div className="chart-container flex items-center gap-2 font-thin">
+              <Chart type="donut" width={20} height={30} data={fatData} />
+              <span className="text-sm font-normal"> {fatData[0].value}g</span>
+            </div>{' '}
+            <div className="chart-container flex items-center gap-2 font-thin">
+              <Chart type="donut" width={20} height={30} data={carbsData} />
+              <span className="text-sm font-normal">
+                {' '}
+                {carbsData[0].value}g
+              </span>
+            </div>
+          </div>
         </div>
-      </CardHeader>
+        <div className="actions flex gap-x-1">
+          <Button
+            variant="outline"
+            size="icon"
+            className="cursor-pointer rounded-full"
+          >
+            <CiEdit className="size-6" />
+          </Button>
+          <Button
+            variant="outline"
+            size="icon"
+            className="cursor-pointer rounded-full"
+          >
+            <FaRegTrashAlt className="size-6" />
+          </Button>
+        </div>
+      </CardContent>
     </Card>
   );
 };
