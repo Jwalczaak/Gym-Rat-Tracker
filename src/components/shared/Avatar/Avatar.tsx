307c999a24f388b470cd4202ef83f366f1fd3e63
Jan Walczak <j.walczaak@gmail.com>
2026-05-17 16:25:51 +0200

    Add hardcode avatar

---
+++ src/components/shared/Avatar/Avatar.tsx
@@ -0,0 +1,12 @@
+const Avatar = () => {
+  return (
+    <div className="border-line flex items-center gap-5 rounded-full border px-3 py-2 text-sm">
+      <div className="to-brand-deep rounded-full bg-linear-to-br from-white px-2 py-2 text-gray-50">
+        <span>JW</span>
+      </div>
+      Jan W.
+    </div>
+  );
+};
+
+export default Avatar;
--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Avatar from '../Avatar/Avatar';
 
 const Header: React.FC = () => {
   return (
-    <div className="border-line flex h-25 items-center gap-5 border-b px-5">
-      <span className="text-fg-muted">Gym Rat Tracker</span>
-      <span className="text-fg-foreground">Diet</span>
+    <div className="border-line flex h-25 items-center justify-between border-b px-10">
+      <div className="flex gap-5">
+        <span className="text-fg-muted">Gym Rat Tracker</span>
+        <span className="text-fg-foreground">Diet</span>
+      </div>
+      <Avatar />
     </div>
   );
 };
